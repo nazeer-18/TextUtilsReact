a19fee43ea6cfe6dea82d2c52795149728d67303
nazeer-18 <ch.en.u4cse21161@ch.students.amrita.edu>
2022-07-19 01:24:56 +0530

    solved issue of word count

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,26 +4,26 @@ export default function TextForm(props) {
     const [text, setText] = useState('');
     const handleUpClick = () => {
         setText(text.toUpperCase());
-        props.displayMsg("converted to upperCase ","success");
+        props.displayMsg("converted to upperCase ", "success");
     }
     const handleLoClick = () => {
         console.log("Convert to Lower case button is clicked " + text);
         setText(text.toLowerCase());
-        props.displayMsg("converted to lowerCase ","success");
+        props.displayMsg("converted to lowerCase ", "success");
     }
     const handleClearClick = () => {
         console.log("clear text is clicked ");
         setText("");
-        props.displayMsg("Cleared text ","warning");
+        props.displayMsg("Cleared text ", "warning");
     }
     const handleCopyClick = () => {
         navigator.clipboard.writeText(text);
-        props.displayMsg("Copied to clipboard  ","success");
+        props.displayMsg("Copied to clipboard  ", "success");
     }
     const handleRemoveClick = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
-        props.displayMsg("Removed extra spaces ","success");
+        props.displayMsg("Removed extra spaces ", "success");
     }
     const handleOnChange = (event) => {
         console.log("on change");
@@ -31,22 +31,22 @@ export default function TextForm(props) {
     }
 
     return (
-        <div style={{color:props.mode==='light'?'black':'white'}} >
+        <div style={{ color: props.mode === 'light' ? 'black' : 'white' }} >
             <h2>{props.heading}  </h2>
             <div className="mb-3">
-                <textarea style={{color:props.mode==='light'?'black':'white',backgroundColor:props.mode==='dark'?'rgb(64 66 88)':'white'}} className={`form-control `} onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+                <textarea style={{ color: props.mode === 'light' ? 'black' : 'white', backgroundColor: props.mode === 'dark' ? 'rgb(64 66 88)' : 'white' }} className={`form-control `} onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
             </div>
-            <button style={{backgroundColor:props.color, color: props.mode!==props.color?"black":props.color , border:"2px solid black"} } className="btn btn-primary mx-2" value="" onClick={handleUpClick} >Convert to Uppercase</button>
-            <button style={{backgroundColor:props.color, color: props.mode!==props.color?"black":props.color , border:"2px solid black"}}  className="btn btn-primary mx-2" onClick={handleLoClick} > Convert to LowerCase </button>
-            <button style={{backgroundColor:props.color, color: props.mode!==props.color?"black":props.color , border:"2px solid black"}}  className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-            <button style={{backgroundColor:props.color, color: props.mode!==props.color?"black":props.color , border:"2px solid black"}}  className="btn btn-primary mx-2" onClick={handleCopyClick}>Copy Text</button>
-            <button style={{backgroundColor:props.color, color: props.mode!==props.color?"black":props.color , border:"2px solid black"}}  className="btn btn-primary mx-2" onClick={handleRemoveClick}>Remove Extra Spaces</button>
+            <button style={{ backgroundColor: props.color, color: props.mode !== props.color ? "black" : props.color, border: "2px solid black" }} className="btn btn-primary mx-2 my-1" value="" onClick={handleUpClick} >Convert to Uppercase</button>
+            <button style={{ backgroundColor: props.color, color: props.mode !== props.color ? "black" : props.color, border: "2px solid black" }} className="btn btn-primary mx-2 my-1" onClick={handleLoClick} > Convert to LowerCase </button>
+            <button style={{ backgroundColor: props.color, color: props.mode !== props.color ? "black" : props.color, border: "2px solid black" }} className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button style={{ backgroundColor: props.color, color: props.mode !== props.color ? "black" : props.color, border: "2px solid black" }} className="btn btn-primary mx-2 my-1" onClick={handleCopyClick}>Copy Text</button>
+            <button style={{ backgroundColor: props.color, color: props.mode !== props.color ? "black" : props.color, border: "2px solid black" }} className="btn btn-primary mx-2 my-1" onClick={handleRemoveClick}>Remove Extra Spaces</button>
             <h2 style={{ textDecoration: "underline", marginLeft: "40%" }} className="my-2">Text Summary</h2>
-            <h4 style={{ float: "left" }} >Word Count : {text.length===0?"0":text.split(" ").length} </h4>
+            <h4 style={{ float: "left" }} >Word Count : {text.split(" ").filter((ele) => { return ele.length !== 0 }).length} </h4>
             <h4 style={{ float: "right" }}> &nbsp;Character Count : {text.length}</h4>
-            <h4 style={{ textAlign: "center" }}>Time Required To Read The Preview Is : {text.length===0?"0":0.008 *text.split(" ").length}</h4>
+            <h4 style={{ textAlign: "center" }}> Minutes Read: {0.008 * text.split(" ").filter((ele) => { return ele.length != 0 }).length}</h4>
             <h4 style={{ textDecoration: "underline", marginLeft: "10px" }}  >Preview :-  </h4>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{text.length===0?"Enter your text in above box to preview it here":text}</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{text.length === 0 ? "Enter your text in above box to preview it here" : text}</p>
 
         </div>
     )
